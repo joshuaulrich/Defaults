c81413f686505016891e7de41cd6187d6b6904f4
Joshua Ulrich <josh.m.ulrich@gmail.com>
2014-11-23 13:23:16 -0600

    Make useDefaults and unDefaults defunct
    Make useDefaults and unDefaults defunct, since they could possibly modify
    functions in other namespaces.  Comment them in all the documentation
    examples (since they now error).
    
    Also remove supporting functions: usingDefaults, .importDefaults,
    useDefaults.original, unDefaults.original.

--- R/getDefaults.R
+++ R/getDefaults.R
@@ -39,7 +39,6 @@ function(calling.fun=NULL) {
     }
   }
 }
-`.importDefaults` <- importDefaults
 `useDefaults` <-
 function(name)
 {
@@ -47,195 +46,20 @@ function(name)
     name <- deparse(substitute(name))
   if(!is.function(eval(parse(text=name))))
     stop("argument 'name' must be a function")
-  if(!identical(grep('importDefaults',deparse(body(name))),integer(0))) invisible(return())
-  cc <- paste('if(exists(".importDefaults")) .importDefaults(calling.fun="',name,'")',sep='')
-  if(deparse(body(name))[1] == "{") {
-    new.body <- as.call(parse(text=c(deparse(body(name))[1],
-                                   cc,
-                                   deparse(body(name))[-1])))[[1]]
-  } else {
-    new.body <- as.call(parse(text=c("{",cc,deparse(body(name))[1],"}")))[[1]]
-  }
-  f <- as.function(c(formals(name),new.body))
-  attributes(f) <- attributes(eval(parse(text=name)))
-  # if in Global, just change Global
-  if(!exists(name,globalenv(),inherits=FALSE)) {
-  # influenced by mtrace code in package:debug
-    # big trick: set environment of new to original
-    environment(f) <- environment(eval(parse(text=name)))
-    locked <- bindingIsLocked(name,as.environment(find(name)))
-    # if binding is Locked unlock, relock...
-    if(locked)
-      unlockBinding(name,as.environment(find(name)))
-    assign(name,f,as.environment(find(name)))
-    if(locked) {
-      ow <- options("warn")
-      on.exit(options(ow))
-      options(warn=-1)
-      lockBinding(name,as.environment(find(name)))
-    }
-  } else {
-    # Global - just reassign
-    environment(f) <- globalenv()
-    assign(name,f,globalenv())
-  }
-  invisible(f)
+  .Defunct("trace", "base",
+    paste0("useDefaults is defunct, because it could potentially modify",
+           " functions in other namespaces. Use trace(", name, ", edit=TRUE)",
+           " to add a call to importDefaults(", name, ") to the first line",
+           " of ", name, "."))
 }
-`usingDefaults`<-
-function(name)
-{
-  if(length(name)==1) {
-    if(is.function(name)) 
-      name <- deparse(substitute(name))
-    try(
-      if(is.function(eval(parse(text=name)))) {
-        return(!identical(grep('importDefaults',deparse(body(name))[2:3]),integer(0)))
-      },silent=TRUE)
-      return(NA)
-  } else {
-    sapply(name,usingDefaults)
-  }
-}
-`useDefaults.original` <-
-function(name)
-{
-  #check for .Defaults <- TRUE to NOT REPEAT
-  env <- as.environment(-1)
-  if(is.function(name))
-    name <- deparse(substitute(name))
 
-  simple.name <- name
-
-  if(!is.function(eval(parse(text=name))))
-    stop("argument must be a function")
-
-  if(identical(grep("(\\.usingDefaults) | (importDefaults)",deparse(body(get(name,
-     env=globalenv())))), integer(0))) { 
-    # only process if not been processes before
-    pkg <- find(name)[1]
-    if(pkg != ".GlobalEnv") {
-      pkg <- strsplit(pkg,":")[[1]][2]
-      ns <- try(loadNamespace(pkg),silent=TRUE)
-      if(isNamespace(ns)) {
-        name <- paste(pkg,"::",name,sep='')
-      }
-    } else {
-      # if defined in global environment, or not a namespace
-      # create new function name .name.CALL in GlobalEnv
-      name <- paste(".",name,".CALL",sep="")
-      # assign to .GlobalEnv for internal call.
-      assign(name,get(simple.name,env=globalenv()),env=globalenv())
-    }
-    new.body <- as.call(parse(text=
-                  c("{",
-                      ".usingDefaults <- TRUE",
-                      "mc <- match.call()[-1]",
-    "all.defaults <- getDefaults(as.character(match.call()[1]))",
-    "passed.args <- names(as.list(mc))",
-    "formal.args <- names(formals(as.character(match.call()[1])))",
-    "default.args <- names(all.defaults)",
-    "final.args <- list()",
-    "for (arg in formal.args) {",
-         "if (!arg %in% passed.args) {",
-             "if (arg %in% default.args) {",
-                 "if (typeof(all.defaults[arg][[1]]) == 'list') {",
-                   "final.args[[arg]] <- as.vector(all.defaults[arg][[1]])",
-                 "}",
-                 "else if (typeof(all.defaults[arg][[1]]) %in% c('symbol', ",
-                   "'language')) {",
-                   "final.args[[arg]] <- all.defaults[arg][[1]]",
-                 "}",
-                 "else if (typeof(all.defaults[arg][[1]]) == 'character') {",
-                   "final.args[[arg]] <- (all.defaults[arg][[1]])",
-                   "if(length(final.args[[arg]])>1)",
-                     "final.args[[arg]] <- as.character(parse(text=final.args[[arg]]))",
-                 "}",
-                 "else {",
-                   "final.args[[arg]] <- as.vector(unlist(all.defaults[arg][[1]]))",
-                 "}",
-             "}",
-         "}",
-         "else {",
-             "if (typeof(as.list(mc)[arg][[1]]) == 'list') {",
-                 "final.args[[arg]] <- as.vector(as.list(mc)[arg][[1]])",
-             "}",
-             "else if (typeof(as.list(mc)[arg][[1]]) %in% c('symbol', ",
-                 "'language')) {",
-                 "final.args[[arg]] <- as.list(mc)[arg][[1]]",
-             "}",
-             "else if (typeof(as.list(mc)[arg][[1]]) == 'character') {",
-                 "final.args[[arg]] <- deparse(as.list(mc)[arg][[1]])",
-             "}",
-             "else {",
-                 "final.args[[arg]] <- as.vector(unlist(as.list(mc)[arg][[1]]))",
-             "}",
-         "}",
-    "}",
-      "if(length(names(final.args))==0) {",
-        "final.args <- ''",
-      "} else {",
-        "final.args <- paste(names(final.args),'=',",
-                            "as.list(final.args),sep='',collapse=',')",
-      "}",
-      paste("eval(parse(text=paste('",name,
-            "(',final.args,')',sep='')),env=sys.parent())",
-            sep=''
-           ),
-    "}")
-    ))[[1]] 
-    assign(simple.name,as.function(c(formals(simple.name),new.body)),env)
-  }
-}
 `unDefaults` <-
 function(name)
 {
   if(is.function(name)) name <- deparse(substitute(name))
-  # if Defaults was hard-coded - return from unDefaults
-  if(identical(grep('\\.importDefaults',deparse(body(name))),integer(0))) {
-    invisible(return())
-  }
-    #stop(paste(dQuote("useDefaults"),"not set for",substitute(name)))
-  old.body <- as.call(parse(text=
-                      deparse(body(name))[-grep('importDefaults',
-                                                deparse(body(name)))]))[[1]]
-  f <- as.function(c(formals(name),old.body))
-  attributes(f) <- attributes(eval(parse(text=name)))
-  if(!exists(name,globalenv(),inherits=FALSE)) {
-    environment(f) <- environment(eval(parse(text=name)))
-    locked <- bindingIsLocked(name,as.environment(find(name)))
-    if(locked)
-      unlockBinding(name,as.environment(find(name)))
-    assign(name,f,as.environment(find(name)))
-    if(locked) {
-      ow <- options("warn")
-      on.exit(options(ow))
-      options(warn=-1)
-      lockBinding(name,as.environment(find(name)))
-    }
-  } else {
-    # Global - just reassign
-    environment(f) <- globalenv()
-    assign(name,f,globalenv())
-  }
-  invisible(f)
-}
-
-"unDefaults.original" <-
-function(name)
-{
-  if(!identical(grep('\\.usingDefaults',deparse(body(name))),
-               integer(0))) {
-    env <- as.environment(-1)
-    if(is.function(name)) name <- deparse(substitute(name))
-    if(exists(paste('.',name,'.CALL',sep=''),env,inherits=FALSE)) {
-      assign(name,get(paste('.',name,'.CALL',sep=''),env),env)
-      remove(list=paste('.',name,'.CALL',sep=''),envir=env)
-    } else {
-      remove(list=name,envir=env)
-    }
-  } else { 
-    stop(paste(dQuote("useDefaults"),"not set for",substitute(name)))
-  }
+  .Defunct("untrace", "base",
+    paste0("unDefaults is defunct, because it could potentially modify",
+           " functions in other namespaces. Use untrace(", name, ") instead."))
 }
 
 `setDefaults` <-
@@ -245,7 +69,7 @@ function (.name, ...)
         .name <- deparse(substitute(.name))
     if(!is.function(eval(parse(text=.name))))
       stop("argument '.name' must be a function")
-    useDefaults(.name)
+
     default.name <- paste(.name, "Default", sep = ".")
     old.defaults <- getDefaults(.name)
     new.defaults <- list(...)
@@ -314,7 +138,6 @@ function(name,confirm=TRUE) {
   if(remove.yes) {
     default.name <- paste(name,"Default",sep=".")
     env <- as.environment(-1)
-    unDefaults(name)
     eval(parse(text=paste('options(',default.name,'=NULL)',sep='')),envir=env)
   }
 }
