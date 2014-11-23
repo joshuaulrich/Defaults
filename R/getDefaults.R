d2ce7a513c5548b1d66b366300f734c1af616326
Joshua Ulrich <josh.m.ulrich@gmail.com>
2014-11-23 12:15:17 -0600

    Add version 1.0-2

--- R/getDefaults.R
+++ R/getDefaults.R
@@ -20,31 +20,48 @@ function(...) {
     }
   }
 }
-
+".importDefaults" <- importDefaults
 "useDefaults" <-
 function(name)
 {
-  if(is.function(name)) name <- deparse(substitute(name))
   env <- as.environment(-1)
-  new.fun.body <- as.call(parse(text=
-                  c(deparse(body(name))[1],
-                  'importDefaults()',
-                  deparse(body(name))[-1])))[[1]]
+  if(is.function(name)) name <- deparse(substitute(name))
+  if(!is.function(eval(parse(text=name))))
+    stop("argument must be a function")
+  # If function already makes use of 'importDefaults()' skip...
+  if(identical(grep('importDefaults()',deparse(body(name))),integer(0))) {
+  if(class(body(name)) == "{") {
+    new.fun.body <- as.call(parse(text=
+                    c(deparse(body(name))[1],
+                    '.importDefaults()',
+                    deparse(body(name))[-1])))[[1]]
+  } else {  # functions lacking enclosures
+    new.fun.body <- as.call(parse(text=
+                    c("{",
+                    '.importDefaults()',
+                    deparse(body(name)),"}")))[[1]]
+  }
   if(exists(name,env,inherits=FALSE))
     assign(paste('.',name,'.orig',sep=''),get(name,env),env)
   assign(name,as.function(c(formals(name),new.fun.body)),env)
+  } 
 }
 
 "unDefaults" <-
 function(name)
 {
-  if(is.function(name)) name <- deparse(substitute(name))
-  env <- as.environment(-1)
-  if(exists(paste('.',name,'.orig',sep=''),env,inherits=FALSE)) {
-    assign(name,get(paste('.',name,'.orig',sep=''),env),env)
-    remove(list=paste('.',name,'.orig',sep=''),envir=env)
-  } else {
-    remove(list=name,envir=env)
+  if(!identical(grep('.importDefaults()',deparse(body(name))),
+               integer(0))) {
+    env <- as.environment(-1)
+    if(is.function(name)) name <- deparse(substitute(name))
+    if(exists(paste('.',name,'.orig',sep=''),env,inherits=FALSE)) {
+      assign(name,get(paste('.',name,'.orig',sep=''),env),env)
+      remove(list=paste('.',name,'.orig',sep=''),envir=env)
+    } else {
+      remove(list=name,envir=env)
+    }
+  } else { 
+    stop(paste(dQuote("useDefaults"),"not set for",substitute(name)))
   }
 }
 
@@ -74,31 +91,69 @@ function(name,...) {
 
 "unsetDefaults" <-
 function(name,confirm=TRUE) {
+  importDefaults()
   if(is.function(name)) name <- deparse(substitute(name))
-  default.name <- paste(name,"Default",sep=".")
+  if(is.null(getDefaults(name))) 
+    stop(paste("no Defaults set for",sQuote(name)))
+  remove.yes <- TRUE
   if(confirm) {
     CONFIRMATION <- readline(prompt=
-            paste("Are you sure you want to remove",name,"defaults? Y/N (N) "))
+            paste("Are you sure you want to remove",
+                  sQuote(name),"defaults? (N): "))
     if(toupper(substr(CONFIRMATION,1,1))!="Y") {
-      warning("Nothing removed!")
-      invisible()
+      remove.yes <- FALSE
+      cat(paste(sQuote(name),"Defaults NOT removed\n"))
+    } else {
+      if(confirm)
+        cat(paste(sQuote(name),"Defaults removed!\n"))
     }
   }
-  env <- as.environment(-1)
-  eval(parse(text=paste('options(',default.name,'=NULL)',sep='')),envir=env)
+  if(remove.yes) {
+    default.name <- paste(name,"Default",sep=".")
+    env <- as.environment(-1)
+    eval(parse(text=paste('options(',default.name,'=NULL)',sep='')),envir=env)
+  }
 }
-
 "getDefaults" <-
-function(name,arg=NULL) {
+function(name=NULL,arg=NULL) {
   if(is.function(name)) name <- deparse(substitute(name))
-  default.name <- paste(name,"Default",sep=".")
-  if(is.null(arg)) {
-    return(options(default.name)[[1]])
-  } else {
-    default.list <- list()
-    for(each.arg in arg) {
-      default.list[[each.arg]] <- options(default.name)[[1]][[each.arg]]
+  if(!is.null(name)) {
+    if(length(name) > 1) {
+      if(!is.character(name))
+        stop(paste(sQuote('name'),"must be a character vector",
+                   "or visible function")) 
+      all.names=list()
     }
-    return(default.list)
+    for(each.name in name) {
+      default.name <- paste(each.name,"Default",sep=".")
+      if(is.null(arg)) {
+        if(exists('all.names',inherit=FALSE)) {
+          all.names[[each.name]] <- options(default.name)[[1]]
+        } else {
+          return(options(default.name)[[1]])
+        }
+      } else {
+        default.list <- list()
+        for(each.arg in arg) {
+          default.list[[each.arg]] <- options(default.name)[[1]][[each.arg]]
+        }
+        if(exists('all.names',inherit=FALSE)) {
+          all.names[[each.name]] <- default.list
+        } else {
+          return(default.list)
+        }
+      }
+    }
+    return(all.names)
+  } else {
+    all.options <- names(options())
+    all.Defaults <-as.character(
+                     sapply(all.options[grep('.Default',all.options)],
+                       FUN=function(x) {
+                         strsplit(x,'\\.')[[1]][[1]]
+                       })
+                   )
+    if(identical(all.Defaults,character(0))) return(NULL)
+    return(all.Defaults)
   }
 }
